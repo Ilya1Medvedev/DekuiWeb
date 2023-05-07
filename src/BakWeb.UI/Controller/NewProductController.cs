c04854c17a68c6b871199ab1c1b323ae2ffc9934
Ilya1Medvedev <ilya.medvedev@stud.vilniustech.lt>
2023-05-07 12:02:22 +0100

    Database bacpac added.

---
+++ src/BakWeb.UI/Controller/NewProductController.cs
@@ -0,0 +1,8 @@
+﻿namespace BakWeb.Controller
+{
+    public class NewProductController
+    {
+        // TODO : On product add, create unique number and store it
+        // TODO : Send email with confirmation that product is added
+    }
+}
